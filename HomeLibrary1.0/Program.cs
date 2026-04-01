4b2cbd65c19c948d921adbd050495b854de53525
DarkWine01 <162964772+DarkWine01@users.noreply.github.com>
2026-04-01 10:59:50 +0300

    Add files via upload
    Версия 1.0

---
+++ HomeLibrary1.0/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Домашняя библиотека");
+
+        List<string> books = new List<string>
+        {
+            "1984",
+            "Мастер и Маргарита",
+            "Гарри Поттер"
+        };
+
+        Console.WriteLine("Список книг:");
+        foreach (var book in books)
+        {
+            Console.WriteLine("- " + book);
+        }
+    }
+}
+
+
+
+
